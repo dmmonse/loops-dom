31b13a2a212f0b08d18f607428cb6247cca4f258
monse <78182277+dmmonse@users.noreply.github.com>
2022-02-18 18:19:43 -0600

    for, while loop, styling with js
    created loops and styled animation

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+//for loop:
+const styles=["bold","italic", "normal"];
+for (Name of styles){
+    console.log (styles);
+if (Name === "italic"){
+    console.log("%cI'm Italic and pink!","font-style:italic; color:pink");
+}
+    if (Name === "bold"){
+        console.log("%cI'm in Bold and yellow!!","font-weight:bold;color:yellow");
+}
+}
+//while loop:
+let full= 0;
+while (full<100){
+    console.log('Page is ready to be used!');
+    full++;
+}
+// styling css with js:
+const text= document.querySelector(".title");
+const animation= document.querySelector(".myAnimation");
 
+function myAnimation() {
+    let id = null;
+    const elem = document.getElementById("animate");   
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        elem.style.top = pos + "px"; 
+        elem.style.left = pos + "px"; 
+      }
+    }
+  }
